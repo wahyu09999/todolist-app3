bf3fcd3ca4a90836b861d3c64c73e54925ae0473
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-04-27 13:35:08 +0700

    login regis

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    use ApiResponse;
+
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return $this->apiSuccess([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+
+
+
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        if(!Auth::attempt($validated)){
+            return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->apiSuccess([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+
+
+
+
+}
---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Contracts\Validation\Validator as ValidationValidator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+
+
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED,
+        ));
+    }
+    
+}
